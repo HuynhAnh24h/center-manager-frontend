3aaf944b512f14d8992a2d34eac1ab5e0c84ee23
HuynhAnh24h <anhdev24h@gmail.com>
2026-04-07 08:22:03 +0700

    Deploy ver 2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-  
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: loadEnv('', process.cwd()).SERVER_URI,
-        changeOrigin: true,
-      },
-    },
-  },
-});
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+    console.log('ENV:', env)
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URI,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  })
+}
